c6bdff87000596ea8c83489ff4beaf2236dfadb7
Borra Anil Teja <123432364+BorraAnilTeja@users.noreply.github.com>
2023-01-27 14:13:42 +0530

    Created POC.java page

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class POC {
+
+	public static void main(String[] args) {
+
+
+		int i = 12345;
+		
+		System.out.println("Creating the POC page for remote master to master local --> local bugfix branch");
+	}
+
+}
